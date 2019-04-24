25effd01ccf3ee976124113d4c1022502e2ed257
Indira <indind052@gmail.com>
2019-04-24 07:33:21 +0600

    fixed

--- src/part01/lesson02/task03/Sex.java
+++ src/part01/lesson02/task03/Sex.java
@@ -2,18 +2,8 @@ package part01.lesson02.task03;
 
 public enum  Sex {
 
-    MAN("man"),
-    WOMAN("woman");
-
-    private final String code;
-
-    Sex(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+    MAN,
+    WOMAN;
 
     public static Sex getSex(boolean value) {
         if (value) {
