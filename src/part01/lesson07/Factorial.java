8ea4e1b425508d497bbfeca5d2383569ca7f8983
Indira <indind052@gmail.com>
2019-05-23 19:27:32 +0600

    lesson 13

---
+++ src/part01/lesson07/Factorial.java
@@ -0,0 +1,28 @@
+package part01.lesson07;
+
+import java.math.BigInteger;
+
+class Factorial implements Runnable {
+
+    private int k;
+    private BigInteger result = BigInteger.ONE;
+
+    public BigInteger getResult() {
+        return result;
+    }
+
+    public Factorial(int k) {
+        this.k = k;
+    }
+
+    public void calculate() {
+        for (int i = 1; i <= k; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+    }
+
+    @Override
+    public void run() {
+        calculate();
+    }
+}
---
+++ src/part01/lesson07/FactorialCounter.java
@@ -0,0 +1,62 @@
+package part01.lesson07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FactorialCounter {
+
+    public int[] getArray() {
+
+        System.out.print("Input array size: ");
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = Integer.valueOf(scanner.nextLine());
+
+        int array[] = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = new Random().nextInt(10000) + 1;
+        }
+
+        return array;
+    }
+
+    public void countInOneThread(int array[]) {
+
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < array.length; i++) {
+            Factorial factorial = new Factorial(array[i]);
+            factorial.calculate();
+        }
+
+        long finishTime = System.nanoTime();
+        double benchTime = (finishTime - startTime) / 1e9;
+        System.out.println("Время выполнения " + benchTime + " секунд");
+
+    }
+
+    public void countWithMultiThread(int array[]) {
+
+        long startTime = System.nanoTime();
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            Thread thread = new Thread(new Factorial(array[i]));
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long finishTime = System.nanoTime();
+        double benchTime = (finishTime - startTime) / 1e9;
+        System.out.println("Время выполнения " + benchTime + " секунд");
+    }
+
+}
